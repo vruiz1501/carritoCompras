67abc6b01207172599b98d9fa38a771b4c8eb68b
Vanessa Ruiz <vruizladino15041@gmail.com>
2024-10-25 21:32:20 -0500

    Carga proyecto

---
+++ app/src/main/java/com/proyectos/carritocompras/ListaCompras.kt
@@ -0,0 +1,20 @@
+package com.proyectos.carritocompras
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListaCompras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lista_compras)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.product_availability)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
